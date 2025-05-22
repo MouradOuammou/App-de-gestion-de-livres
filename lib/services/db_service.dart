de0fad1f843b7ab6c37455afe1c6e149944224f8
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-22 15:30:13 +0100

    ajouter dep de http et sqlf

---
+++ lib/services/db_service.dart
@@ -0,0 +1,66 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import '../models/book.dart';
+
+class DatabaseService {
+  static Database? _database;
+  static const String _tableName = 'favorites';
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    final path = join(await getDatabasesPath(), 'books_database.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async {
+        await db.execute('''
+          CREATE TABLE $_tableName (
+            id TEXT PRIMARY KEY,
+            title TEXT,
+            author TEXT,
+            imageUrl TEXT
+          )
+        ''');
+      },
+    );
+  }
+
+  Future<void> insertBook(Book book) async {
+    final db = await database;
+    await db.insert(
+      _tableName,
+      book.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<List<Book>> getBooks() async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query(_tableName);
+
+    return List.generate(maps.length, (i) {
+      return Book.fromMap(maps[i]);
+    });
+  }
+
+  Future<void> deleteBook(String id) async {
+    final db = await database;
+    await db.delete(
+      _tableName,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+  Future<void> close() async {
+    final db = await database;
+    await db.close();
+  }
+}
