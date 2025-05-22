3e05efdcf41cca10e2e873956977b07bd524b351
Mourad Ouammou <mouradouammou8@gmail.com>
2025-05-22 15:12:39 +0100

    code de 1ere service

---
+++ lib/services/api_service.dart
@@ -0,0 +1,20 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import '../models/book.dart';
+
+class ApiService {
+  static const String _baseUrl = 'https://www.googleapis.com/books/v1/volumes';
+
+  Future<List<Book>> searchBooks(String query) async {
+    final response = await http.get(Uri.parse('$_baseUrl?q=$query'));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final items = data['items'] as List<dynamic>? ?? [];
+
+      return items.map((item) => Book.fromJson(item)).toList();
+    } else {
+      throw Exception('Failed to load books');
+    }
+  }
+}
